e5b765de894ef067c6144c1336ca316a37047fd3
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-15 17:25:51 +0000

    Commit

--- api/add-employee/__init__.py
+++ api/add-employee/__init__.py
@@ -44,6 +44,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     #conn.commit()
 
     return func.HttpResponse(
-        req_body.get_json(),
+        req.get_json(),
         status_code=200
     )
