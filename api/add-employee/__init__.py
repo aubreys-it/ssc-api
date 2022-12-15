b8574792f07acc4a8a06c4087dedf8a166afd2b4
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-15 17:01:26 +0000

    Commit

--- api/add-employee/__init__.py
+++ api/add-employee/__init__.py
@@ -11,30 +11,28 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     except ValueError:
         pass
 
-    sql = "INSERT INTO ssc.insert_server_info VALUES "
-    sql += "(" + req_body['locId'] + "),"
-    sql += "('" + req_body['firstName'] + "'),"
-    sql += "('" + req_body['lastName'] + "'),"
-    sql += "('" + req_body['displayName'] + "'),"
-    sql += "('" + req_body['phone'] + "'),"
-    sql += "('" + req_body['emailAddress'] + "'),"
-    sql += "('" + req_body['abcExpirationDate'] + "'),"
-    sql += "(" + req_body['monAM'] + "),"
-    sql += "(" + req_body['monPM'] + "),"
-    sql += "(" + req_body['tueAM'] + "),"
-    sql += "(" + req_body['tuePM'] + "),"
-    sql += "(" + req_body['wedAM'] + "),"
-    sql += "(" + req_body['wedPM'] + "),"
-    sql += "(" + req_body['thuAM'] + "),"
-    sql += "(" + req_body['thuPM'] + "),"
-    sql += "(" + req_body['friAM'] + "),"
-    sql += "(" + req_body['friPM'] + "),"
-    sql += "(" + req_body['satAM'] + "),"
-    sql += "(" + req_body['satPM'] + "),"
-    sql += "(" + req_body['sunAM'] + "),"
-    sql += "(" + req_body['sunPM'] + "),"
-    sql += "('" + req_body['tfd'] + "'),"
-    sql += "('" + req_body['ttd'] + "');"
+    items = []
+    quotedItems = ['firstName', 'lastName', 'displayName', 'phone', 'emailAddress', 'abcExpireDate', 'tfd', 'ttd']
+
+    for item in req_body:
+        items.append(item)
+
+    sql = "INSERT INTO ssc.insert_server_info ("
+    for item in items:
+        sql += item + ","
+
+    sql = sql[:len(sql)-1] + ") VALUES ("
+
+    for item in items:
+        if item in quotedItems:
+            sql += "'" + req_body[item] + "',"
+        else:
+            sql += req_body[item] + ","
+
+    sql = sql[:len(sql)-1]
+
+    sql += ");"
+    
     
     #onn = pyodbc.connect(os.environ['DMCP_CONNECT_STRING'])
     #cursor = conn.cursor()
