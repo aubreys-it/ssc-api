9c3d43b1dd958ad9aacddf8dda5d3c4d5d5c96bb
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-16 18:15:56 +0000

    Commit

--- api/delete-employee/__init__.py
+++ api/delete-employee/__init__.py
@@ -7,7 +7,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
-    allParams = json.dumps(req.params)
     if not name:
         try:
             req_body = req.get_json()
