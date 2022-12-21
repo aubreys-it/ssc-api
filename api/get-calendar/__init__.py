9156a68907ee7da9fcd4f823e916a530c9dae4e7
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-21 14:16:26 +0000

    Commit

--- api/get-calendar/__init__.py
+++ api/get-calendar/__init__.py
@@ -3,7 +3,6 @@ import os
 import pyodbc
 import json
 import azure.functions as func
-from datetime import date
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
