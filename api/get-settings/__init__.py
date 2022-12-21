1810a6b81f9ea365c01ac810bd5070676ad4d4ac
granthopperflash <98110041+granthopperflash@users.noreply.github.com>
2022-12-21 15:21:52 +0000

    Commit

---
+++ api/get-settings/__init__.py
@@ -0,0 +1,46 @@
+import logging
+import os
+import pyodbc
+import json
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    locId = req.params.get('locId')
+    if not locId:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            locId = req_body.get('locId')
+
+    sql = "SELECT * FROM ssc.schedule_settings WHERE locId=" + locId + " ORDER BY shiftId;"
+
+    conn = pyodbc.connect(os.environ['DMCP_CONNECT_STRING'])
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    cursor.close()
+    conn.close()
+
+    shifts = {}
+
+    for row in rows:
+        if not row[7] in shifts.keys():
+            shifts[row[7]] = {}
+            shifts[row[7]]['shiftName'] = row[0]
+            shifts[row[7]]['mustCallNeeded'] = row[1]
+            shifts[row[7]]['openShifts'] = row[2]
+            shifts[row[7]]['amTogo'] = row[3]
+            shifts[row[7]]['amBartenders'] = row[4]
+            shifts[row[7]]['autoRotateBar'] = row[5]
+            shifts[row[7]]['inTimes'] = row[6]
+            shifts[row[7]]['dayNumber'] = row[8]
+            shifts[row[7]]['dayPart'] = row[9]
+            shifts[row[7]]['locId'] = row[10]
+
+    if shifts:
+        return func.HttpResponse(json.dumps(shifts))
+    
